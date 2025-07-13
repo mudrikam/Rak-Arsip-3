8bc93c15694ce8d528d0adde5438c9a16d93e086
mudrikam <mudrikulh@gmail.com>
2025-07-13 19:39:42 +0700

    feat: Update placeholder text for project name input in NameFieldWidget

--- gui/widgets/name_field.py
+++ gui/widgets/name_field.py
@@ -20,6 +20,7 @@ class NameFieldWidget(QFrame):
         self.line_edit = QLineEdit(self)
         self.line_edit.setMinimumHeight(40)
         self.line_edit.setStyleSheet("font-size: 20px;")
+        self.line_edit.setPlaceholderText("Project Name...")
         input_row.addWidget(self.line_edit)
 
         self.make_btn = QPushButton("Make", self)
