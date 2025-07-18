ee91637991a3a75d15ffb51c0c3e1e2fe603a4b3
mudrikam <mudrikulh@gmail.com>
2025-07-18 13:34:51 +0700

    feat: Implement code changes to enhance functionality and improve performance

--- Launcher.bat
+++ Launcher.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdDWDJHuR/U40FClVR0mjLnizFvhP1Pr+/NaVo0YUX+0xNobY1dQ=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlTMbyXqZg==
+::ZQ05rAF9IAHYFVzEqQICKRAUbRaRNXv6VdU=
+::eg0/rx1wNQPfEVWB+kM9LVsJDDaDNyubFKYV+Kiojw==
+::fBEirQZwNQPfEVWB+kM9LVsJDDaDNyubFKYV+Kiojw==
+::cRolqwZ3JBvQF1fEqQICKRAUbRaRNXv6VdU=
+::dhA7uBVwLU+EWH2B50M5JhJVLA==
+::YQ03rBFzNR3SWATE0EcjKRJaRQXi
+::dhAmsQZ3MwfNWATE0EcjKRJaRQXi
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDWDJHuR/U40FClVR0mjLnizFvhP1Nr65KqmsF4URKJsNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal
 
