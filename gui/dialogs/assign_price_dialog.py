aa4ab064ffb96362e7927e1a28c1b42a95a4f550
mudrikam <mudrikulh@gmail.com>
2025-07-30 10:31:19 +0700

    refactor: Remove unnecessary table refresh call in AssignPriceDialog on accept

--- gui/dialogs/assign_price_dialog.py
+++ gui/dialogs/assign_price_dialog.py
@@ -415,5 +415,4 @@ class AssignPriceDialog(QDialog):
         batch_list = [self.batch_combo.itemText(i) for i in range(self.batch_combo.count())]
         if batch_number and client_id and batch_number in batch_list:
             self.db_manager.assign_file_client_batch(file_id, client_id, batch_number)
-        self._parent.refresh_table()
         self.accept()
